d02078b2939044aee0bbc0aa8cca89b0a52326a5
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-17 12:17:30 -0300

    j

--- Accounts/index.js
+++ Accounts/index.js
@@ -4,6 +4,7 @@ const chalk = require('chalk')
 
 //modulos internos
 const fs = require('fs')
+const { Console } = require('console')
 
 operation()
 
@@ -22,6 +23,19 @@ function operation(){
     if(action==='Criar Conta'){
         createAccount()
     }
+    else if(action==='Depositar'){
+        deposit()
+    }
+    else if(action==='Consultar Saldo'){
+
+    }
+    else if(action==='Sacar'){
+
+    }
+    else if(action==='Sair'){
+        console.log(chalk.bgBlue.black('Obrigado por usar o Accounts'))
+        process.exit()
+    }
  })
     .catch(err => console.log(err))
 }
@@ -30,4 +44,77 @@ function operation(){
 function createAccount(){
     console.log(chalk.bgGreen.white('Obrigado por escolher nosso serviço'))
     console.log(chalk.green('Defina as opções da sua conta bancária a seguir'))
+    buildAccount()
+}
+
+function buildAccount(){
+
+    inquirer.prompt([
+        {
+            name:"accountName",
+            message:"Digite Seu nome Para cadastrarmos",
+        },
+    ]).then((resp)=>{
+        const accountName = resp["accountName"]
+
+        console.info(accountName)
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Esta conta ja existe. Por Favor Insira Outro nome'))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`,'{"balance":0}',function(err){console.log(err)},)
+
+        console.log(chalk.green('Conta criada com sucesso'))
+    })
+    .catch(err=> console.log(err))
+
+}
+
+function deposit(){
+
+    inquirer.prompt([
+        {
+            name:'accountName',
+            message:'Qual o nome da sua conta',
+        },
+    ]).then((resp)=>{
+        const accountName = resp['accountName']
+        console.info(accountName)
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            inquirer.prompt([
+                {
+                    name:'balance',
+                    message:'Coloque a quantia desejada que gostaria de depositar',
+                },
+            ]).then((resp)=>{
+                const balance = resp['balance']
+                
+            })
+            .catch(err=>console.log(err))
+        }
+        else if(!fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('o Nome informado (Não existe) ou (foi inserido errado) tente novamente'))
+            deposit()
+            return
+        }
+        
+        
+    })
+    .catch(err=> console.log(err))
+
+
+}
+
+function checkAccount(accountName){
+
+    if(!fs.existsSync(`accounts/${accountName}.json`)){
+        Console.log(chalk.bgRed.black('o Nome informado (Não existe) ou (foi inserido errado) tente novamente'))
+        return false
+    }
+    return true
 }
