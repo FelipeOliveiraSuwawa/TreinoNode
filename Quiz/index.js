be49d0eb2ac6b4fc45e0f35b8fd807c6e89d68b9
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-26 15:26:38 -0300

    k

--- Quiz/index.js
+++ Quiz/index.js
@@ -2,6 +2,18 @@ const inquirer = require('inquirer')
 const chalk = require('chalk')
 const fs = require('fs')
 
+const question = [
+
+    {
+        per1:'lerolero',
+        resp1:['yes','haha','no'],
+    },
+    {
+        per2:'fornite?',
+        resp:['ben','speed','lala']
+    }
+]
+
 
 startOperation()
 
@@ -34,4 +46,24 @@ function playGame(){
 
     console.log(chalk.blue('the rule is simple mark the correct option'))
     const pontos = 0
+    mostrarPergunta(question)
 }
+
+ function mostrarPergunta(e){
+
+    inquirer.prompt([
+        {
+            type:'list',
+            name:'action',
+            message:'',
+            choices:['haha','lala']
+
+        }
+    ]).then((resp)=>{
+        const action = resp['action']
+
+        console.log(action)
+    })
+    .catch(err => console.log(err))
+
+ }
