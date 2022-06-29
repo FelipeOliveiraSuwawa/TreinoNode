bde52a515ad406e90793c79284cb411e48180eaa
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-29 19:43:26 -0300

    k

--- Quiz/index.js
+++ Quiz/index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer')
 const chalk = require('chalk')
 const fs = require('fs')
 
-
+let moedas = 25
 
 startOperation()
 
@@ -51,4 +51,29 @@ function startOperation(){
             }
         })
         .catch(err=>console.log(err))
+    }
+
+    function goShop(){
+        inquirer.prompt([
+            {
+                type:'list',
+                name:'action',
+                message:'What you gonna buy',
+                choices:['Sword C=25','Dagger C=20','Armor C=50','Exit'],
+            },
+        ]).then((resp)=>{
+            const action = resp['action']
+
+            if(action === 'Sword C=25' && moedas == 25 || action ==='Dagger C=20' && moedas == 20){
+                console.log(chalk.green('The purchase was a success'))
+                if(action ==='Sword C=25'){moedas -= 25}
+                console.log(moedas)
+                return startOperation()
+            }
+                else{
+                    console.log(chalk.red('aa vai trabalha meu brother'))
+                }
+                      
+        })
+        .catch(err=> console.log(err))
     }
